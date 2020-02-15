ac11a3c2aa2cc66447bd88cd855d42cdabaaf557
Cerko <wangjunchao-hj@huajiao.tv>
2020-02-15 11:40:13 +0800

    MyObj3

---
+++ GitPatchDemo/classes/MyObj3.m
@@ -0,0 +1,13 @@
+//
+//  MyObj3.m
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/15.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import "MyObj3.h"
+
+@implementation MyObj3
+
+@end
