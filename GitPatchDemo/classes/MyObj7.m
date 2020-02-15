9677947a333d520ae1536effd286f4b74e811cfc
Cerko <wangjunchao-hj@huajiao.tv>
2020-02-15 12:56:56 +0800

    MyObj7

---
+++ GitPatchDemo/classes/MyObj7.m
@@ -0,0 +1,13 @@
+//
+//  MyObj7.m
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/15.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import "MyObj7.h"
+
+@implementation MyObj7
+
+@end
