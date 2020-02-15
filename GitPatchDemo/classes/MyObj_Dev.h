537b1a1d6e8da1db4c5e7d8ceb091cf5e91d24dc
Cerko <wangjunchao-hj@huajiao.tv>
2020-02-15 12:32:23 +0800

    add my obj dev

---
+++ GitPatchDemo/classes/MyObj_Dev.h
@@ -0,0 +1,17 @@
+//
+//  MyObj_Dev.h
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/15.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyObj_Dev : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitPatchDemo/classes/MyObj_Dev.m
@@ -0,0 +1,13 @@
+//
+//  MyObj_Dev.m
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/15.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import "MyObj_Dev.h"
+
+@implementation MyObj_Dev
+
+@end
