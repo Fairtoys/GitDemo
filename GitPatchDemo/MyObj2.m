89276a81e39fc5cd5ee81a8f2b56c5d2188c9b5a
Cerko <wangjunchao-hj@huajiao.tv>
2020-02-15 11:32:49 +0800

    add myobj2

---
+++ GitPatchDemo/MyObj2.m
@@ -0,0 +1,13 @@
+//
+//  MyObj2.m
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/15.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import "MyObj2.h"
+
+@implementation MyObj2
+
+@end
