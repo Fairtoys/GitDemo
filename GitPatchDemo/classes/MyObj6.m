c80d6fbe6e61ed5eae78a573aa5a41153a4c255a
Cerko <wangjunchao-hj@huajiao.tv>
2020-02-15 12:34:46 +0800

    add my obj 6

---
+++ GitPatchDemo/classes/MyObj6.m
@@ -0,0 +1,13 @@
+//
+//  MyObj6.m
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/15.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import "MyObj6.h"
+
+@implementation MyObj6
+
+@end
