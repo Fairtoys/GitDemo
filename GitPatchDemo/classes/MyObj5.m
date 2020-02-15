49c262a63ad91ac7af2499f5b0f5f0235ba9c6a2
Cerko <wangjunchao-hj@huajiao.tv>
2020-02-15 12:33:21 +0800

    add obj 5

---
+++ GitPatchDemo/classes/MyObj5.m
@@ -0,0 +1,13 @@
+//
+//  MyObj5.m
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/15.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import "MyObj5.h"
+
+@implementation MyObj5
+
+@end
