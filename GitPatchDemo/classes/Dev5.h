9c56cbb687f35f2fa854660d9fa95ee28139a590
Cerko <wangjunchao-hj@huajiao.tv>
2020-02-16 10:22:41 +0800

    add dev5

---
+++ GitPatchDemo/classes/Dev5.h
@@ -0,0 +1,17 @@
+//
+//  Dev5.h
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/16.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dev5 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitPatchDemo/classes/Dev5.m
@@ -0,0 +1,13 @@
+//
+//  Dev5.m
+//  GitPatchDemo
+//
+//  Created by Cerko on 2020/2/16.
+//  Copyright © 2020 Cerko. All rights reserved.
+//
+
+#import "Dev5.h"
+
+@implementation Dev5
+
+@end
